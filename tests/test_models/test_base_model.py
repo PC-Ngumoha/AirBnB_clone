f4cf4d59ca24a75bd6e9b5780520a6337e50cec9
PC-Ngumoha <pcngumoha@gmail.com>
2022-08-31 13:21:16 +0000

    Update test file 'tests/test_models/test_base_model.py'

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,16 @@
-'''
-Test file for the 'BaseModel' class
+'''Test file for the 'BaseModel' class
 '''
 
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
-class TestBaseModel(unittest.Testcase):
-	'''
-	TestBaseModel test class: contains the test functions
+class TestBaseModel(unittest.TestCase):
+	'''TestBaseModel test class: contains the test functions
 	Each test function represents a single test case.
 	'''
 	def test_if_public_attribute(self):
-		'''
-		tests if the 'id', 'created_at' and 'updated_at' instance attributes
+		'''tests if the 'id', 'created_at' and 'updated_at' instance attributes
 		are public instance attributes.
 		'''
 		base1 = BaseModel()
@@ -28,23 +25,20 @@ class TestBaseModel(unittest.Testcase):
 				raise AttributeError
 
 	def test_string_format(self):
-		'''
-		tests the format of the string representation of a 'BaseModel' object
+		'''tests the format of the string representation of a 'BaseModel' object
 		'''
 		base1 = BaseModel()
 		test_string = f"[{base1.__class__.__name__}] ({base1.id}) {base1.__dict__}"
 		self.assertEqual(str(base1), test_string)
 
 	def test_id_type(self):
-		'''
-		tests id's type to see if it's a string
+		'''tests id's type to see if it's a string
 		'''
 		base1 = BaseModel()
 		self.assertIs(type(base1.id), str)
 
 	def test_current_datetime(self):
-		'''
-		tests if the 'created_at' and 'updated_at' attributes of a 'BaseModel' instance
+		'''tests if the 'created_at' and 'updated_at' attributes of a 'BaseModel' instance
 		are set to the current time.
 		'''
 		base1 = BaseModel()
@@ -56,8 +50,7 @@ class TestBaseModel(unittest.Testcase):
 	
 
 	def test_id_exists_dict(self):
-		'''
-		tests if the 'id' element exists in the object's dictionary representation.
+		'''tests if the 'id' element exists in the object's dictionary representation.
 		and has a unique ID value
 		'''
 		base1 = BaseModel()
@@ -66,8 +59,7 @@ class TestBaseModel(unittest.Testcase):
 		self.assertIn('id', obj_dict.keys())
 
 	def test_class_in_dict(self):
-		'''
-		tests if the '__class__' key was added to the dictionary representation
+		'''tests if the '__class__' key was added to the dictionary representation
 		of the 'BaseModel' object.
 		'''
 		base1 = BaseModel()
@@ -75,20 +67,18 @@ class TestBaseModel(unittest.Testcase):
 		self.assertIn('__class__', obj_dict.keys())
 
 	def test_created_and_updated_time_format(self):
-		'''
-		tests the string format of the 'created_at' and 'updated_at' keys in the
+		'''tests the string format of the 'created_at' and 'updated_at' keys in the
 		dictionary representation of the 'BaseModel' object.
 		'''
 		base1 = BaseModel()
-		obj_dict = base1.to_dict()
 		format_created = base1.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 		format_updated = base1.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+		obj_dict = base1.to_dict()
 		self.assertEqual(obj_dict['created_at'], format_created)
 		self.assertEqual(obj_dict['updated_at'], format_updated)
 
 	def test_updated_time(self):
-		'''
-		tests if the 'updated_at' attribute's value is updated whenever the save()
+		'''tests if the 'updated_at' attribute's value is updated whenever the save()
 		function is called.
 		'''
 		base1 = BaseModel()
