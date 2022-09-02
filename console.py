20a61cddb23c5e442c60592cd51003b63bb97eee
PC-Ngumoha <pcngumoha@gmail.com>
2022-09-02 11:17:58 +0000

    Minor Additions

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ from cmd import Cmd
 
 class HBNBCommand(Cmd):
     prompt = '(hbnb) '
+    class_names = ('BaseModel', )
 
     def emptyline(self):
         '''Is called when an empty line is passed to the console.
@@ -21,6 +22,31 @@ class HBNBCommand(Cmd):
         '''Exits when CTRL+D on the keyboard is pressed
         '''
         return True
+
+    def do_create(self, line):
+        '''Creates a new instance of a specified class.
+        '''
+        pass
+
+    def do_show(self, line):
+        '''Prints the string representation of an instance with the exact id
+        '''
+        pass
+
+    def do_destroy(self, line):
+        '''Deletes an instance based on the class name and id
+        '''
+        pass
+
+    def do_all(self, line):
+        '''Prints the string form of all instances of the specified class name
+        '''
+        pass
+
+    def update(self, line):
+        '''Updates an instance based on a class name and id.
+        '''
+        pass
 
 
 if __name__ == '__main__':
