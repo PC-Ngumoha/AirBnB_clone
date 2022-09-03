6e6ae132e659d79b726b941391aa8bf26d8ef247
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-09-03 14:05:02 +0000

    Update to tests/test_model/test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -26,20 +26,40 @@ class TestUser(unittest.TestCase):
         '''
         self.assertTrue(hasattr(User, 'email'))
 
+    def test_email_type(self):
+        '''Test if `email` attribute is of type str
+        '''
+        self.assertIsInstance(User.email, str)
+
     def test_password_exists(self):
         '''Tests that `User` object has password attribute
         '''
         self.assertTrue(hasattr(User, 'password'))
+
+    def test_password_type(self):
+        '''Test if `password` attribute is of type str
+        '''
+        self.assertIsInstance(User.password, str)
 
     def test_first_name_exists(self):
         '''Tests that `User` object has first_name attribute
         '''
         self.assertTrue(hasattr(User, 'first_name'))
 
+    def test_first_name_type(self):
+        '''Test if `first_name` attribute is of type str
+        '''
+        self.assertIsInstance(User.first_name, str)
+
     def test_last_name_exists(self):
         '''Tests that `User` object has last_name attribute
         '''
         self.assertTrue(hasattr(User, 'last_name'))
+
+    def test_last_name_type(self):
+        '''Test if `last_name` attribute is of type str
+        '''
+        self.assertIsInstance(User.last_name, str)
 
 
 if __name__ == "__main__":
